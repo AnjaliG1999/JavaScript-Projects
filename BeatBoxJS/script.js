830d68c7fa935c48fb617823088cbfc7d991e6a6
Anjali1999 <anjali24.7goyal@gmail.com>
2020-09-10 10:41:40 +0530

    Completed the project, added required functionality and visual effects

--- BeatBoxJS/script.js
+++ BeatBoxJS/script.js
@@ -1,30 +1,39 @@
 let beats = {
     "65": {
-        beat: new Beat("./assets/piano_D.mp3")
+        beat: new Beat("./assets/piano_D.mp3"),
+        button: new Button("#00fffe", 65)
     },
     "83": {
-        beat: new Beat("./assets/piano_C_sharp.mp3")
+        beat: new Beat("./assets/piano_C_sharp.mp3"),
+        button: new Button("#00fffe", 83)
     },
     "68": {
-        beat: new Beat("./assets/piano_A.mp3")
+        beat: new Beat("./assets/piano_A.mp3"),
+        button: new Button("#00fffe", 68)
     },
-    "65": {
-        beat: new Beat('./assets/Drum Sticks Hit.mp3')
+    "70": {
+        beat: new Beat('./assets/Drum Sticks Hit.mp3'),
+        button: new Button("#ff00ff", 70)
     },
     "71": {
-        beat: new Beat("./assets/Snare Drum Roll.mp3")
+        beat: new Beat("./assets/Snare Drum Roll.mp3"),
+        button: new Button("#ff00ff", 71)
     },
     "72": {
-        beat: new Beat("./assets/Bass Drum.mp3")
+        beat: new Beat("./assets/Bass Drum.mp3"),
+        button: new Button("#ff00ff", 72)
     },
     "74": {
-        beat: new Beat("./assets/Cymbal suspended.mp3")
+        beat: new Beat("./assets/Cymbal suspended.mp3"),
+        button: new Button("#ff00ff", 74)
     },
     "75": {
-        beat: new Beat("./assets/Basic Beat1.mp3")
+        beat: new Beat("./assets/Basic Beat1.mp3"),
+        button: new Button("#ffffff", 75)
     },
     "76": {
-        beat: new Beat("./assets/Basic Beat2.mp3")
+        beat: new Beat("./assets/Basic Beat2.mp3"),
+        button: new Button("#ffffff", 76)
     }
 }
 
@@ -32,10 +41,10 @@ triggerBeat = (event) => {
     const key = event.keyCode;
     console.log(key)
     if(key in beats) {
-        beats[key].beat.play()
+        beats[key].beat.play();
+        beats[key].button.select();
     }
-    
 }
 
 
-document.addEventListener('keydown', triggerBeat)
+document.addEventListener('keydown', triggerBeat);
--- BeatBoxJS/utils.js
+++ BeatBoxJS/utils.js
@@ -1,7 +1,7 @@
 class Beat {
     constructor(fileLink) {
         this.audio = new Audio(fileLink);
-        console.log(this.audio)
+        // console.log(this.audio)
         // this.play()
     }
 
@@ -11,22 +11,32 @@ class Beat {
     }
 }
 
-console.log()
+// console.log()
 
 class Button{
     constructor(color, keycode) {
-
+        this.color = color;
+        this.keycode = keycode;
+        this.element = document.getElementById(keycode);
+        console.log(this.element)
+        this.setButtonColorInHTML()
+        // this.select()
     }
     setButtonColorInHTML = () => {
-
+        console.log(this.element)
+        this.element.style.borderColor = this.color;
     }
 
     select = () => {
-        
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+        this.element.addEventListener("transitionend", this.deselect)
+        // setTimeout(this.deselect, 3000)
     }
 
     deselect = () => {
-        
+        this.element.style.backgroundColor = ''; // or 'transparent'
+        this.element.style.boxShadow = ''; // or 'none'
     }
 }
 
