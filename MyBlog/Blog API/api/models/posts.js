76f47065f26b0b30e65625c26bac4d7396ff7efc
AnjaliG1999 <anjali24.7goyal@gmail.com>
2020-09-20 13:16:13 +0530

    Set up api end points

---
+++ MyBlog/Blog API/api/models/posts.js
@@ -0,0 +1,36 @@
+const PATH = "../data.json"
+const fs = require('fs');
+
+class Post {
+    get() {
+        // get posts
+        return this.readData();
+    }
+
+    getIndividualPosts(postId) {
+        // get one blog post
+        const posts = this.readData();
+        const findPost = posts.find(post => post.id == postId);
+        return findPost;
+    }
+
+    add(newPost) {
+        // add new post
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(currentPosts);
+    }
+
+    readData() {
+        let rawdata = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawdata);
+        return posts;
+    }
+
+    storeData(rawdata) {
+        let data = JSON.stringify(rawdata);
+        fs.writeFileSync(PATH, data);
+    }
+}
+
+module.exports = Post; 
--- MyBlog/Blog API/uploads/app.js
+++ MyBlog/Blog API/uploads/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
-
-const posts = [{
-    "id": "1581461442206",
-    "title": "This is a New Blog Post",
-    "content": "This is the content! ",
-    "post_image": "uploads/post-image-1581461442199.jpg",
-    "added_date": "1581461442206"
-}]
+const Post = require("../api/models/posts");
+const postsData = new Post();
 
 app.get("/api/posts", (req, res) => {
-    res.status(200).send(posts)
+
+    // const test = {
+    //     testing: "testing"
+    // }
+    // postsData.add(test);
+    res.status(200).send(postsData.get());
+})
+
+app.get("/api/posts/:post_id", (req, res) => {
+    const postId = req.params.post_id;
+    const foundPost = postsData.getIndividualPosts(postId);
+    if(foundPost){
+        res.status(200).send(foundPost);
+    }else{
+        res.status(404).send('Not found');
+    }
+    
 })
 
 app.listen(3000, () => console.log("Listening to localhost!"));
